bae8ad21ceb8d73fcb2e926bdce5209d080d5f88
yeraySG <yeray.guzman@gmail.com>
2024-12-19 12:18:22 +0100

    Added new scenario, refactored some commands to make the overall structure cleaner

--- cypress/e2e/techTest/employeesTest.cy.js
+++ cypress/e2e/techTest/employeesTest.cy.js
@@ -4,41 +4,41 @@ describe('Validate cities on table', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/employees.html')
   })
-  it ('Page loads properly', () => {
+  it('Page loads properly', () => {
     cy.contains('Login')
     cy.get('#treeGrid').should('exist')
     cy.get('#form').should('be.visible')
 
   })
-  
-  it('Cities are displayed on the grid', () =>{
+
+  it('Cities are displayed on the grid', () => {
     const expectedCities = ['Tacoma', 'Seattle', 'Kirkland', 'Redmond', 'London', '']
-    
+
     cy.expandTreeButton('Steven')
 
-    cy.get('#jqxScrollThumbhorizontalScrollBartreeGrid') 
-      .trigger('mousedown', { which: 1 })  
-      .trigger('mousemove', { clientX: 600, clientY: 0 }) 
+    cy.get('#jqxScrollThumbhorizontalScrollBartreeGrid')
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientX: 600, clientY: 0 })
       .trigger('mouseup')
 
     const foundCities = new Set()
 
     cy.get('#treeGrid')
-      .find('tr') 
+      .find('tr')
       .each(($row) => {
         cy.wrap($row)
-          .find('td') 
-          .last() 
+          .find('td')
+          .last()
           .invoke('text')
           .then((cityText) => {
-            foundCities.add(cityText.trim()) 
+            foundCities.add(cityText.trim())
           })
       })
       .then(() => {
         expectedCities.forEach((city) => {
           expect(foundCities.has(city)).to.be.true
+        })
       })
-    })
   })
 })
 
@@ -48,50 +48,47 @@ describe('Checkbox scenarios', () => {
   })
 
   it('No employees are selected', () => {
-    cy.get('#btn')
-      .click()
+    cy.clickViewSelectedData()
 
     cy.get('#listBoxContentlistBoxSelected')
-      .should('be.empty') 
+      .should('be.empty')
   })
 
-  it ('Select all employees', () => {
+  it('Select all employees', () => {
     cy.expandTreeButton('Steven')
 
     let selectedEmployees = [];
 
     cy.get('#treeGrid')
       .find('.jqx-tree-grid-checkbox')
-      .each(($checkbox, index) => {  
+      .each(($checkbox, index) => {
         cy.get('#treeGrid')
           .find('.jqx-tree-grid-checkbox')
-          .eq(index)  
+          .eq(index)
           .click()
-      // .click({ multiple: true })  // This one also works but might be not as safe
 
         cy.get('#treeGrid')
           .find('tr')
           .eq(index)
           .within(() => {
             cy.get('td').eq(0).invoke('text').then((firstName) => {
-                cy.get('td').eq(3).invoke('text').then((city) => {
-                  selectedEmployees.push({ name: `${firstName}`, city });
-                })
+              cy.get('td').eq(3).invoke('text').then((city) => {
+                selectedEmployees.push({ name: `${firstName}`, city });
               })
             })
           })
+      })
 
-          cy.get('#btn')
-            .click()
- 
+    cy.clickViewSelectedData()
+
     cy.get('#listBoxSelected')
-      .find('.jqx-listitem-state-normal') 
+      .find('.jqx-listitem-state-normal')
       .each(($li, index) => {
         const listItemText = $li.text()
         const expectedEmployee = selectedEmployees[index]
 
-        expect(listItemText).to.include(expectedEmployee.name) 
-        expect(listItemText).to.include(expectedEmployee.city) 
+        expect(listItemText).to.include(expectedEmployee.name)
+        expect(listItemText).to.include(expectedEmployee.city)
       })
   })
 
@@ -99,21 +96,66 @@ describe('Checkbox scenarios', () => {
 
     cy.expandTreeButton('Steven')
 
+    cy.selectRowsWithCity('')
+
+    cy.clickViewSelectedData()
+
+    cy.get('#listBoxContentlistBoxSelected')
+      .should('contain', null)
+  })
+
+  it('Multiple checks', () => {
+
+    let cityNames = []
+
     cy.get('#treeGrid')
       .find('tr') 
-      .then(($rows) => {
-        const emptyCityRow = $rows.toArray().find(row => {
-          const cityText = Cypress.$(row).find('td').eq(3).text().trim()
-          return cityText === ''
-        })
-      if (emptyCityRow) {
-        cy.wrap(emptyCityRow)
-          .find('.jqx-tree-grid-checkbox') 
-          .click();
-        cy.get('#btn')
-          .click()
-        cy.get('#listBoxContentlistBoxSelected')
-          .should('contain', null) 
-          }})
-        })
+      .eq(1) 
+      .find('td') 
+      .eq(3) 
+      .invoke('text') 
+      .then((cityText) => {
+        cityNames.push(cityText.trim())
       })
+
+    cy.checkRow(1)
+    cy.clickViewSelectedData()
+
+
+    cy.get('#listBoxSelected') 
+      .find('.jqx-listitem-state-normal') 
+      .each(($li, index) => {
+        const listItemText = $li.text()
+
+        expect(listItemText).to.include(cityNames[index]);
+      })
+
+    cy.get('#treeGrid')
+      .find('tr')
+      .eq(0) 
+      .find('td')
+      .eq(3) 
+      .invoke('text')
+      .then((cityText) => {
+        cityNames.push(cityText.trim())
+      })
+
+    cy.checkRow(0)
+
+    cy.clickViewSelectedData()
+
+    cy.get('#listBoxSelected') 
+      .find('.jqx-listitem-state-normal') 
+      .each(($li, index) => {
+        const listItemText = $li.text()
+        
+        let foundCity = false;
+        cityNames.forEach((city) => {
+          if (listItemText.includes(city)) {
+            foundCity = true
+          }
+        })
+        expect(foundCity).to.be.true
+      })
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,35 @@ Cypress.Commands.add('expandTreeButton', (nodeName) => {
       .siblings('.jqx-tree-grid-collapse-button')
       .click()
 })
+
+Cypress.Commands.add('selectRowsWithCity', (cityValue) => {
+    cy.get('#treeGrid')
+      .find('tr')  
+      .then(($rows) => {
+        const rowToSelect = $rows.toArray().find(row => {
+            const cityText = Cypress.$(row).find('td').eq(3).text().trim()
+            
+            return cityText === cityValue
+        })
+        if (rowToSelect) {
+            cy.wrap(rowToSelect)
+              .find('.jqx-tree-grid-checkbox') 
+              .click()
+            }
+          })
+      })
+
+
+Cypress.Commands.add('clickViewSelectedData', () => {  
+    cy.get('#btn')
+      .click() 
+    })    
+
+Cypress.Commands.add('checkRow', (row) => { 
+    cy.get('#treeGrid')
+        .find('tr')
+        .eq(row)
+        .find('.jqx-tree-grid-checkbox') 
+        .click() 
+    }) 
+    
